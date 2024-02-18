50f50cb53d59ca1319b31b980238cf2d23dd6213
khajiitos <khajiit112@gmail.com>
2024-02-18 07:03:42 +0100

    1.1.0

---
+++ Common/src/main/java/me/khajiitos/worldplaytime/common/config/WPTConfigValues.java
@@ -0,0 +1,76 @@
+package me.khajiitos.worldplaytime.common.config;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+
+public class WPTConfigValues {
+
+    public static abstract class Value<T> {
+        private T value;
+        private final T defaultValue;
+
+        private Value(T defaultValue) {
+            this.defaultValue = defaultValue;
+            this.value = defaultValue;
+        }
+
+        public T get() {
+            return this.value;
+        }
+
+        public void set(T value) {
+            this.value = value;
+        }
+
+        public void setUnchecked(Object obj) {
+            this.value = (T)obj;
+        }
+
+        public T getDefault() {
+            return this.defaultValue;
+        }
+
+        public abstract T read(JsonElement jsonElement);
+        public abstract JsonElement write();
+    }
+
+    public static class BooleanValue extends Value<Boolean> {
+        public BooleanValue(Boolean defaultValue) {
+            super(defaultValue);
+        }
+
+        @Override
+        public Boolean read(JsonElement jsonElement) {
+            return jsonElement.isJsonPrimitive() ? jsonElement.getAsJsonPrimitive().getAsBoolean() : this.getDefault();
+        }
+
+        @Override
+        public JsonElement write() {
+            return new JsonPrimitive(this.get());
+        }
+    }
+
+    public static class EnumValue<T extends Enum<?>> extends Value<T> {
+        public EnumValue(T defaultValue) {
+            super(defaultValue);
+        }
+
+        @Override
+        public T read(JsonElement jsonElement) {
+            String name = jsonElement.getAsString();
+
+            for (Enum<?> enumConstant : this.getDefault().getClass().getEnumConstants()) {
+                if (enumConstant.name().equalsIgnoreCase(name)) {
+                    return (T) enumConstant;
+                }
+            }
+
+            return this.getDefault();
+        }
+
+        @Override
+        public JsonElement write() {
+            return new JsonPrimitive(this.get().name().toLowerCase());
+        }
+    }
+}
--- Forge/src/main/java/me/khajiitos/worldplaytime/forge/WorldPlayTimeForge.java
+++ Forge/src/main/java/me/khajiitos/worldplaytime/forge/WorldPlayTimeForge.java
@@ -1,34 +1,34 @@
-package me.khajiitos.worldplaytime.neoforged;
+package me.khajiitos.worldplaytime.forge;
 
 import me.khajiitos.worldplaytime.common.WorldPlayTime;
 import me.khajiitos.worldplaytime.common.config.cloth.ClothConfigCheck;
 import me.khajiitos.worldplaytime.common.config.cloth.ClothConfigScreenMaker;
 import me.khajiitos.worldplaytime.common.handler.EventHandlerCommon;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.fml.ModLoadingContext;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.loading.FMLLoader;
-import net.neoforged.neoforge.client.ConfigScreenHandler;
-import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.GameShuttingDownEvent;
-import net.neoforged.neoforge.event.TickEvent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.GameShuttingDownEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod(WorldPlayTime.MOD_ID)
-public class WorldPlayTimeNeoForged {
-    public WorldPlayTimeNeoForged() {
-        if (FMLLoader.getDist() == Dist.CLIENT) {
+public class WorldPlayTimeForge {
+    public WorldPlayTimeForge() {
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
             WorldPlayTime.init();
 
-            NeoForge.EVENT_BUS.addListener(WorldPlayTimeNeoForged::onClientTick);
-            NeoForge.EVENT_BUS.addListener(WorldPlayTimeNeoForged::onShutDown);
-            NeoForge.EVENT_BUS.addListener(WorldPlayTimeNeoForged::onLoggedOut);
+            MinecraftForge.EVENT_BUS.addListener(WorldPlayTimeForge::onClientTick);
+            MinecraftForge.EVENT_BUS.addListener(WorldPlayTimeForge::onShutDown);
+            MinecraftForge.EVENT_BUS.addListener(WorldPlayTimeForge::onLoggedOut);
+
 
             if (ClothConfigCheck.isInstalled()) {
                 ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory(ClothConfigScreenMaker::create));
             }
-
-        }
+        });
     }
 
     private static void onClientTick(TickEvent.ClientTickEvent e) {
